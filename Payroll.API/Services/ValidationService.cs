762e0c406736e4e06944896940c8c1ccb5005250
azhadolfo <azhadolfo1@gmail.com>
2026-03-09 10:38:08 +0800

    feat: implement the fluentvalidation and global exception handler

---
+++ Payroll.API/Services/ValidationService.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace Payroll.API.Services
+{
+    public class ValidationService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ValidationService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task ValidateAsync<T>(T model)
+        {
+            var validator = _serviceProvider.GetService<IValidator<T>>();
+
+            if (validator is null)
+                return;
+
+            var result = await validator.ValidateAsync(model);
+
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
---
+++ Payroll.API/Validators/CreateDepartmentValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Payroll.API.Dtos.Department;
+
+namespace Payroll.API.Validators
+{
+    public class CreateDepartmentValidator : AbstractValidator<DepartmentCreateDto>
+    {
+        public CreateDepartmentValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+        }
+    }
+}
